e0feb05e030e63bd20a41920cdf99a4e3e4a4cc4
Sierra Williams <78832175+sierrawillliams@users.noreply.github.com>
2024-01-19 13:46:02 -0800

    font size

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,7 +13,7 @@ function Main() {
           <source src={backgroundVideo} type='video/mp4' alt='Video by 33studiodesign from Pixabay'/>
         </video>
         <div className='responsive-type-animation'>
-          <TypeAnimation className='name' sequence={["Sierra Williams", 1000]} speed={20} style={{ fontSize: '8vw', fontFamily: 'Bright', textShadow: '3px 2px #f67c90', color: '#fff'}}/>
+          <TypeAnimation className='name' sequence={["Sierra Williams", 1000]} speed={20} style={{ fontSize: '12vw', fontFamily: 'Bright', textShadow: '3px 2px #f67c90', color: '#fff'}}/>
         </div> 
         <p className='title'>Web Developer</p>
         <div className='main-btns'>
