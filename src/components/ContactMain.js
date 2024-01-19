a40ac2a4dea0b83637623aa5a34b07ed12a43835
Sierra Williams <78832175+sierrawillliams@users.noreply.github.com>
2024-01-19 14:38:31 -0800

    titles changed

--- src/components/ContactMain.js
+++ src/components/ContactMain.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import backgroundVideo from '../videos/wave1.mp4';
+import { TypeAnimation } from 'react-type-animation';       
+
 
 function ContactMain() {
   return (
@@ -7,7 +9,9 @@ function ContactMain() {
         <video autoPlay loop muted playsInline>
             <source src={backgroundVideo} type='video/mp4' alt='Video by PCh Art from Pixabay'/>
         </video>
-        <h1>CONTACT</h1>
+        <div className='responsive-type-animation'>
+          <TypeAnimation className='name' sequence={["Contact", 1000]} speed={20} style={{ fontSize: '12vw', fontFamily: 'Bright', textShadow: '3px 2px #f67c90', color: '#fff'}}/>
+        </div> 
     </div>
   )
 }
--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -3,6 +3,8 @@ import '../../App.css';
 import Cards from '../Cards';
 import Footer from '../Footer';
 import backgroundVideo from '../../videos/background.mp4';
+import { TypeAnimation } from 'react-type-animation';       
+
 
 export default function Work() {
     return <> 
@@ -10,7 +12,9 @@ export default function Work() {
         <video autoPlay loop muted playsInline>
             <source src={backgroundVideo} type='video/mp4' alt='Video by Jemaver from Pixabay'/>
         </video>
-        <h1>WORK</h1>
+        <div className='responsive-type-animation'>
+          <TypeAnimation className='name' sequence={["Work", 1000]} speed={20} style={{ fontSize: '12vw', fontFamily: 'Bright', textShadow: '3px 2px #f67c90', color: '#fff'}}/>
+        </div> 
     </div>
     <Cards/>
     <Footer/>
